6735432cef357a4ffdbea6f9c898bac18a20f1e7
ignaciogatti <nacho.gatti@gmail.com>
2019-03-08 17:32:52 -0300

    Changes to run app in a flexible enviroment

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
-
+from werkzeug.contrib.fixers import ProxyFix
 from .config import config_by_name
 
 def create_app(config_name):
     #setup and configuration
     app = Flask(__name__)
+    app.wsgi_app = ProxyFix(app.wsgi_app)
     app.config.from_object(config_by_name[config_name])
 
     from app.main import main as main_blueprint
